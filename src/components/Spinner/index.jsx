941e13e1acba5578529b8f4bf011905cc1cda5e0
thiago <thiagosilva128@gmail.com>
2021-09-18 22:01:56 -0300

    created a spinner

---
+++ src/components/Spinner/index.jsx
@@ -0,0 +1,10 @@
+import { ImSpinner2 } from 'react-icons/im'
+import { ContainerSpinner } from './styles'
+
+export function Spinner() {
+    return (
+        <ContainerSpinner>
+            <ImSpinner2 />
+        </ContainerSpinner>
+    )
+}
---
+++ src/components/Spinner/styles.js
@@ -0,0 +1,23 @@
+import styled, { keyframes } from 'styled-components'
+
+
+const rotate = keyframes`
+    from {
+        transform: rotate(0deg);
+    }
+    to {
+        transform: rotate(360deg);
+    } 
+`
+
+export const ContainerSpinner = styled.p`
+    width: 100%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    animation: ${rotate} 1s infinite linear;
+
+`
+
